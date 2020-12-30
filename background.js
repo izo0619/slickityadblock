0c62fdeb8abe1235d970f4943b9d9cac4fbb3ec2
izo0619 <isabelzhong30@gmail.com>
2020-12-30 15:57:10 -0800

    init

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details){
+        console.log("I am going to block:", details.url)
+        return {cancel: true}},
+    { urls: blocked_sites},
+    ["blocking"]
+)
